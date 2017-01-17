1b9f89fd7025b0b55e21dfc31e813094277e24b2
Abel.shi <abel.shi@derbysoft.com>
2017-01-17 16:11:16 +0800

    创建spring web项目，更改配置文件和目录结构

---
+++ spring-project/src/main/java/com/sf/web/exception/DefaultMappingExceptionResolver.java
@@ -0,0 +1,10 @@
+package com.sf.web.exception;
+
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
+
+/**
+ * Created by SF on 2017/1/17.
+ */
+public class DefaultMappingExceptionResolver extends SimpleMappingExceptionResolver {
+
+}
