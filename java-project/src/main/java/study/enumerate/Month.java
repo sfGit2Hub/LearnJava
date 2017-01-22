03a5d80b459bcdc2aafb1309eee7e45fed9ad83e
Abel.shi <abel.shi@derbysoft.com>
2017-01-22 11:01:35 +0800

    增加月份 枚举类

---
+++ java-project/src/main/java/study/enumerate/Month.java
@@ -0,0 +1,23 @@
+package study.enumerate;
+
+/**
+ * Created by SF on 2017/1/22.
+ */
+public enum Month {
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December;
+
+    public static void main(String []args) {
+        System.out.println(Month.April);
+    }
+}
