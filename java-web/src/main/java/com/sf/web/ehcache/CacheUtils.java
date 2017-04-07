8b9182c8081bd67ca33b304cf6e7b80e29a1775d
Abel.shi <abel.shi@derbysoft.com>
2017-04-07 14:56:59 +0800

    ehcache add flush to disk

--- java-web/src/main/java/com/sf/web/ehcache/CacheUtils.java
+++ java-web/src/main/java/com/sf/web/ehcache/CacheUtils.java
@@ -1,5 +1,6 @@
 package com.sf.web.ehcache;
 
+import common.use.Person;
 import net.sf.ehcache.Cache;
 import net.sf.ehcache.CacheManager;
 import net.sf.ehcache.Element;
@@ -12,18 +13,30 @@ public class CacheUtils {
     private static Cache cache;
 
     static {
-        cacheManager = CacheManager.create(CacheUtils.class.getClassLoader().getResourceAsStream("echcache.xml"));
+        cacheManager = CacheManager.create(CacheUtils.class.getClassLoader().getResourceAsStream("ehcache.xml"));
         cache = cacheManager.getCache("myCache");
     }
 
     public static void put(String key, Object value) {
         Element element = new Element(key, value);
         cache.put(element);
+        cache.flush();
     }
 
     public static <T> T get(String key, Class<T> typeClass) {
         Element element = cache.get(key);
-        return element == null ? null : (T) element.getObjectValue();
+        return element == null ? null : (T) element.getValue();
     }
 
+    public static void main(String[] args) {
+        Person jack = new Person().setName("jack").setAge(20);
+        CacheUtils.put("jack", jack);
+        System.out.println("put cache in");
+
+        Person cacheJack = CacheUtils.get("jack", Person.class);
+        System.out.println(cacheJack);
+
+//        cacheManager.cacheExists("myCache");
+//        cacheManager.shutdown();
+    }
 }
