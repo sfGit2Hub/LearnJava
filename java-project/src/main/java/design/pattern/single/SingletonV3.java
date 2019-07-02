ef5e09f116521f05ff4d77105795f6c18381a539
fshi <fshi@ctrip.com>
2019-07-02 23:08:30 +0800

    single pattern update

--- java-project/src/main/java/design/pattern/single/SingletonV3.java
+++ java-project/src/main/java/design/pattern/single/SingletonV3.java
@@ -14,7 +14,10 @@ public class SingletonV3 {
         if (uniqueSingleton == null){
             //只有在为null的时候再加锁，会减少很多性能损耗
             synchronized (SingletonV3.class){
-                uniqueSingleton = new SingletonV3();
+                //内部再判断一次是否null，避免加锁的时候有线程已经创建了实例
+                if (uniqueSingleton == null) {
+                    uniqueSingleton = new SingletonV3();
+                }
             }
         }
         return uniqueSingleton;
